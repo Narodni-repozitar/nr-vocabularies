e6449f8ffc705cdd0da5abc62b08cb427d979300
Mirek Simek <miroslav.simek@gmail.com>
2022-07-25 18:11:01 +0200

    local install

--- local_install.sh
+++ local_install.sh
@@ -5,12 +5,6 @@ cd $(dirname $0)
 source .venv/bin/activate
 
 (
-  cd oarepo-vocabularies
+  cd nr-vocabularies
   pip install -e '.[tests,elasticsearch7,postgresql]'
 )
-
-(
-  cd oarepo-vocabularies-basic
-  pip install -e .
-)
-
